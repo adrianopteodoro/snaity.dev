ae7b3344e90e2b490aee9bf0287bc12feff28c44
Adriano Teodoro <adrianopteodoro@gmail.com>
2023-12-31 22:10:30 -0300

    fix export

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /**
-* @type {import('next').NextConfig}
-*/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
-  };
-  
-  export default nextConfig;
+  output: "export",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
+
+export default nextConfig;
